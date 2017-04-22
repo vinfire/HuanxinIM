f857279a91ccccd0baf04c4b9bea6ccbbe8d6bba
Vinfire <vinfire4th@163.com>
2017-04-22 22:00:01 +0800

    first commit

--- app/src/main/java/com/example/gtr/huanxinim/controller/activity/LoginActivity.java
+++ app/src/main/java/com/example/gtr/huanxinim/controller/activity/LoginActivity.java
@@ -2,12 +2,13 @@ package com.example.gtr.huanxinim.controller.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.gtr.huanxinim.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener{
 
     private EditText et_login_name;
     private EditText et_login_password;
@@ -23,6 +24,13 @@ public class LoginActivity extends Activity {
         et_login_password = (EditText) findViewById(R.id.et_login_password);
         btn_login_regist = (Button) findViewById(R.id.btn_login_regist);
         btn_login_login = (Button) findViewById(R.id.btn_login_login);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
