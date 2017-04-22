0f823bfe0f351e4cbdd6423e6a8306916693c5d3
Vinfire <vinfire4th@163.com>
2017-04-22 22:02:38 +0800

    first commit

---
+++ app/src/main/java/com/example/gtr/huanxinim/controller/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.gtr.huanxinim.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gtr.huanxinim.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
